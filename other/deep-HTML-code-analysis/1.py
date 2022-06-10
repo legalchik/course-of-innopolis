9ff1e8bffbc13ade3c665c971ed7d6c28be62b6d
legalchik <93884665+legalchik@users.noreply.github.com>
2022-06-10 23:30:46 +0300

    Delete other directory

--- other/deep-HTML-code-analysis/1.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup 
-import requests
-
-page = requests.get("https://www.pythonscraping.com/pages/warandpeace.html")
-soup = BeautifulSoup(page.text, "html.parser")
-
-for el in soup.findAll(class_="red"):
-	print(el.text)
-	
